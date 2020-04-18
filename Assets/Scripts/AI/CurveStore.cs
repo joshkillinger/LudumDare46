8d9fe08a2118af70cd1398ba8cccd0ed2d5af1bc
joshkillinger <josh.killinger@gmail.com>
2020-04-17 21:30:07 -0600

    Adding CurveStore for storing curves.

---
+++ Assets/Scripts/AI/CurveStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace AI
+{
+  [CreateAssetMenu(fileName = "CurveStore", menuName = "Scriptables/CurveStore")]
+  public class CurveStore : ScriptableObject
+  {
+    [Serializable]
+    public struct CurveEntry
+    {
+      public string Name;
+      public AnimatorController Animator;
+    }
+
+    public List<CurveEntry> Entries;
+  }
+}

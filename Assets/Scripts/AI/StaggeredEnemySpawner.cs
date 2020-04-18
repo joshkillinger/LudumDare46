27d37a35405bbfaa93023f26307c1400b1331e29
joshkillinger <josh.killinger@gmail.com>
2020-04-18 01:14:23 -0600

    Basic spawner, demo wave, and pathing enemies.

---
+++ Assets/Scripts/AI/StaggeredEnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace AI
+{
+  public class StaggeredEnemySpawner : MonoBehaviour
+  {
+    public Animator Prefab;
+    public AnimatorController path;
+    public int MinCount;
+    public int MaxCount;
+    public float Stagger;
+
+    private IEnumerator Start()
+    {
+      int count = Random.Range(MinCount, MaxCount);
+
+      for (int i = 0; i < count; ++i)
+      {
+        var instance = Instantiate(Prefab, transform);
+        instance.runtimeAnimatorController = path;
+        
+        yield return new WaitForSeconds(Stagger);
+      }
+    }
+  }
+}
---
+++ Assets/Scripts/DestroyAfterSeconds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+  public class DestroyAfterSeconds : MonoBehaviour
+  {
+    public float TimeToLive = 5f;
+    
+    private void Start()
+    {
+      StartCoroutine(delay());
+    }
+
+    private IEnumerator delay()
+    {
+      yield return new WaitForSeconds(TimeToLive);
+      Destroy(gameObject);
+    }
+  }
+}

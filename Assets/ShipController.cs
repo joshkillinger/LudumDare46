401513bbe1a0a45ccddf992ed74a0ddd47f6d99b
Jacob Etter <jurgenetter@gmail.com>
2020-04-17 20:20:59 -0600

    Adding basic camera follow and forward movement.

---
+++ Assets/ShipController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public float speed = 10f;
+    private float velocity = 0;
+    public float boostMult = 1.2f;
+
+    public bool go = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (go)
+        {
+            velocity = speed; //TODO: smooth ramp up/ ramp down
+
+            if (Input.GetAxis("Boost") > 0)
+            {
+                velocity *= boostMult;
+            }
+
+            this.transform.position += Time.deltaTime * velocity * this.transform.forward;
+
+
+
+        }
+    }
+}

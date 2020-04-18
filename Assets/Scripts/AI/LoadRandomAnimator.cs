db1a8537bdf1fe2bd93069b0e7041cd3065ca6d7
joshkillinger <josh.killinger@gmail.com>
2020-04-17 23:51:25 -0600

    Adding LoadRandomAnimator script

---
+++ Assets/Scripts/AI/LoadRandomAnimator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor.Animations;
+using UnityEngine;
+
+namespace AI
+{
+  public class LoadRandomAnimator : MonoBehaviour
+  {
+    public CurveStore curves;
+
+    private AnimatorController animator;
+    
+    private void Start()
+    {
+      animator = curves.Entries[Random.Range(0, curves.Entries.Count)].Animator;
+    }
+  }
+}

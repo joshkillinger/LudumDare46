4f59083a440727c2f82f13621dd13eb4b71291e9
Jacob Etter <jurgenetter@gmail.com>
2020-04-19 14:36:10 -0600

    Can now switch to two different guns.  Rapid Fire Laser and Shotgun

---
+++ Assets/Scripts/SetGunValuesFromPreset.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+  [Serializable]
+  public struct GunPreset
+  {
+    public GameObject shot;
+
+    public float energyMax;
+    public float regenerationRate;
+    public float drainAmount;
+    public float cooldown;
+    public float maxDeviation;
+    public float shotAmount;
+  }
+  public class SetGunValuesFromPreset : MonoBehaviour
+  {
+    public GunController gunController;
+    public List<GunPreset> gunPresets;
+
+    public void Event_SwapToGunPreset(int index)
+    {
+      var preset = gunPresets[index];
+
+      gunController.shot = preset.shot; 
+      gunController.energyMax = preset.energyMax;
+      gunController.regenerationRate = preset.regenerationRate;
+      gunController.drainAmount = preset.drainAmount;
+      gunController.cooldown = preset.cooldown;
+      gunController.maxDeviation = preset.maxDeviation;
+      gunController.shotAmount = preset.shotAmount;
+    }
+  }
+}
